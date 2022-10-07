7d1cfec3aec6623a0c35bc77830f4349f070e7e0
Nataliia <75252511+NataliiaValko@users.noreply.github.com>
2022-10-07 04:57:10 +0300

    11

--- src/js/simplelightbox.js
+++ src/js/simplelightbox.js
@@ -1,4 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
