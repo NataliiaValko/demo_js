31ec29485dd5c21654c6eab0f813f294af8cbea9
Nataliia <75252511+NataliiaValko@users.noreply.github.com>
2022-11-19 15:04:14 +0200

    adds arrows to slick

--- src/js/simplelightbox.js
+++ src/js/simplelightbox.js
@@ -6,11 +6,9 @@ const lightbox = new SimpleLightbox('.gallery a', {
 // https://simplelightbox.com/
 // https://cdnjs.com/libraries/simplelightbox
 
-// 1. В терміналі прописати команду "npm install simplelightbox"
+// 1. Створити у папці js окремий файл, наприклад, "simplelightbox.js" і вставити в нього цей код
 
-// 2. Створити у папці js окремий файл, наприклад, "simplelightbox.js" і вставити в нього цей код
-
-// 3. Підключити у head
+// 2. Підключити у head
 //  <link
 //   rel="stylesheet"
 //   href="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.10.4/simple-lightbox.min.css"
@@ -19,10 +17,10 @@ const lightbox = new SimpleLightbox('.gallery a', {
 //   referrerpolicy="no-referrer"
 // />
 
-// 4. Підключити перед закриваючим тегом </body> наступні лінки
+// 3. Підключити перед закриваючим тегом </body> наступні лінки
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.10.4/simple-lightbox.min.js"
 // integrity="sha512-IKDNPsyNuvwDr8D2nIcJVgNFuj3XXdV0jJ5S6E+nYwl5RiLxrd51TnDJDN1lQJrVdF4K849Lizr12ZCZV81MoA=="
 // crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-// 5. Задати клас "gallery" на батьківський елемент (div чи ul) та огорнути усі зображення галереї у посилання. В атрибут src зображення вказати шлях до малого зображення, а в атрибут href посилання -- шлях до великого.
+// 4. Задати клас "gallery" на батьківський елемент (div чи ul) та огорнути усі зображення галереї у посилання. В атрибут src зображення вказати шлях до малого зображення, а в атрибут href посилання -- шлях до великого.
 //Після цього, при кликі на зображення, воно буде збільшуватися
